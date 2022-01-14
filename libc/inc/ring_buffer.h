455c5552234db5b57d0d1b0914a84561df07be21
Enrico Fraccaroli (Galfurian) <enrico.fraccaroli@univr.it>
2022-01-14 09:12:27 -0500

    Improve ring-buffer definition.

--- libc/inc/ring_buffer.h
+++ libc/inc/ring_buffer.h
@@ -5,47 +5,63 @@
 
 #pragma once
 
-#define DECLARE_FIXED_SIZE_RING_BUFFER(type, name, length, init)                            \
-    typedef struct fs_rb_##name##_t {                                                       \
-        unsigned size, read, write;                                                         \
-        type buffer[length];                                                                \
-    } fs_rb_##name##_t;                                                                     \
-    static inline void fs_rb_##name##_init(fs_rb_##name##_t *rb)                            \
-    {                                                                                       \
-        rb->size = length;                                                                  \
-        rb->read = rb->write = 0;                                                           \
-        char *dst            = (char *)rb->buffer;                                          \
-        long num             = sizeof(type) * length;                                       \
-        while (num--) *dst++ = (char)(init & 0xFF);                                         \
-    }                                                                                       \
-    static inline unsigned fs_rb_##name##_step(fs_rb_##name##_t *rb, unsigned index)        \
-    {                                                                                       \
-        return (index == (rb->size - 1)) ? 0 : index + 1;                                   \
-    }                                                                                       \
-    static inline void fs_rb_##name##_push(fs_rb_##name##_t *rb, type item)                 \
-    {                                                                                       \
-        if (fs_rb_##name##_step(rb, rb->write) == rb->read)                                 \
-            rb->read = fs_rb_##name##_step(rb, rb->read);                                   \
-        rb->buffer[rb->write] = item;                                                       \
-        rb->write             = fs_rb_##name##_step(rb, rb->write);                         \
-    }                                                                                       \
-    static inline type fs_rb_##name##_empty(fs_rb_##name##_t *rb)                           \
-    {                                                                                       \
-        return rb->write == rb->read;                                                       \
-    }                                                                                       \
-    static inline type fs_rb_##name##_pop(fs_rb_##name##_t *rb)                             \
-    {                                                                                       \
-        type item = init;                                                                   \
-        if (rb->write != rb->read) {                                                        \
-            item     = rb->buffer[rb->read];                                                \
-            rb->read = fs_rb_##name##_step(rb, rb->read);                                   \
-        }                                                                                   \
-        return item;                                                                        \
-    }                                                                                       \
-    static inline void fs_rb_##name##_get(fs_rb_##name##_t *rb, unsigned index, type *item) \
-    {                                                                                       \
-        if (index < rb->size)                                                               \
-            *item = rb->buffer[index];                                                      \
+#define DECLARE_FIXED_SIZE_RING_BUFFER(type, name, length, init)                     \
+    typedef struct fs_rb_##name##_t {                                                \
+        unsigned size, read, write;                                                  \
+        type buffer[length];                                                         \
+    } fs_rb_##name##_t;                                                              \
+    static inline void fs_rb_##name##_init(fs_rb_##name##_t *rb)                     \
+    {                                                                                \
+        rb->size = length;                                                           \
+        rb->read = rb->write = 0;                                                    \
+        char *dst            = (char *)rb->buffer;                                   \
+        long num             = sizeof(type) * length;                                \
+        while (num--) *dst++ = (char)(init & 0xFF);                                  \
+    }                                                                                \
+    static inline unsigned fs_rb_##name##_step(fs_rb_##name##_t *rb, unsigned index) \
+    {                                                                                \
+        return (index == (rb->size - 1)) ? 0 : index + 1;                            \
+    }                                                                                \
+    static inline void fs_rb_##name##_push_front(fs_rb_##name##_t *rb, type item)    \
+    {                                                                                \
+        if (fs_rb_##name##_step(rb, rb->write) == rb->read)                          \
+            rb->read = fs_rb_##name##_step(rb, rb->read);                            \
+        rb->buffer[rb->write] = item;                                                \
+        rb->write             = fs_rb_##name##_step(rb, rb->write);                  \
+    }                                                                                \
+    static inline type fs_rb_##name##_empty(fs_rb_##name##_t *rb)                    \
+    {                                                                                \
+        return rb->write == rb->read;                                                \
+    }                                                                                \
+    static inline type fs_rb_##name##_pop_back(fs_rb_##name##_t *rb)                 \
+    {                                                                                \
+        type item = init;                                                            \
+        if (!fs_rb_##name##_empty(rb)) {                                             \
+            item     = rb->buffer[rb->read];                                         \
+            rb->read = fs_rb_##name##_step(rb, rb->read);                            \
+        }                                                                            \
+        return item;                                                                 \
+    }                                                                                \
+    static inline type fs_rb_##name##_get(fs_rb_##name##_t *rb, unsigned index)      \
+    {                                                                                \
+        if (index < rb->size)                                                        \
+            return rb->buffer[index];                                                \
+        return init;                                                                 \
+    }                                                                                \
+    static inline type fs_rb_##name##_back(fs_rb_##name##_t *rb)                     \
+    {                                                                                \
+        if (!fs_rb_##name##_empty(rb))                                               \
+            return rb->buffer[rb->read];                                             \
+        return init;                                                                 \
+    }                                                                                \
+    static inline type fs_rb_##name##_front(fs_rb_##name##_t *rb)                    \
+    {                                                                                \
+        if (!fs_rb_##name##_empty(rb)) {                                             \
+            if (rb->write > 0)                                                       \
+                return rb->buffer[rb->write - 1];                                    \
+            return rb->buffer[rb->size - 1];                                         \
+        }                                                                            \
+        return init;                                                                 \
     }
 
 #ifdef __KERNEL__
