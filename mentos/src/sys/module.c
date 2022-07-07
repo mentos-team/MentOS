3a56fb41d51b45097c7af4852360686352680132
Enrico Fraccaroli (Galfurian) <enry.frak@gmail.com>
2022-07-07 11:36:14 -0400

    Correctly react to MULTIBOOT_FLAG_MODS saying there are no modules to load.

--- mentos/src/sys/module.c
+++ mentos/src/sys/module.c
@@ -30,7 +30,7 @@ int init_modules(multiboot_info_t *header)
         modules[i].pad       = 0;
     }
     if (!bitmask_check(header->flags, MULTIBOOT_FLAG_MODS))
-        return 0;
+        return 1;
     multiboot_module_t *mod = first_module(header);
     for (int i = 0; (mod != 0) && (i < MAX_MODULES);
          ++i, mod = next_module(header, mod)) {
