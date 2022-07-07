821135d686bc67b902eb65c2a9645e6ab5218668
Enrico Fraccaroli (Galfurian) <enry.frak@gmail.com>
2022-07-07 10:51:25 -0400

    Add missing mkdir in programs compilation flow. Add check for executables in script.

--- scripts/get_text_address.sh
+++ scripts/get_text_address.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 for obj_file in "$@"
 do
-  ADDR=$(readelf -WS $obj_file | grep .text | awk '{ print "0x"$5 }')
-  echo "add-symbol-file $obj_file ${ADDR}"
+  if [[ -x "$obj_file" ]]
+  then
+    ADDR=$(readelf -WS $obj_file | grep .text | awk '{ print "0x"$5 }')
+    echo "add-symbol-file $obj_file ${ADDR}"
+  fi
 done
