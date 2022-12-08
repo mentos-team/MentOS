a64ce2f81d1e581744b37cce4f406b0272635251
Enrico Fraccaroli <enry.frak@gmail.com>
2022-12-08 11:28:17 -0500

    Update version.

--- mentos/inc/version.h
+++ mentos/inc/version.h
@@ -18,10 +18,10 @@
 #define OS_MAJOR_VERSION 0
 
 /// Minor version of the operating system.
-#define OS_MINOR_VERSION 4
+#define OS_MINOR_VERSION 5
 
 /// Micro version of the operating system.
-#define OS_MICRO_VERSION 0
+#define OS_MICRO_VERSION 1
 
 /// Helper to transform the given argument into a string.
 #define OS_STR_HELPER(x) #x
