e23c434cc6e00a2e12060a8cc7628b95c962609e
Enrico Fraccaroli (Galfurian) <enry.frak@gmail.com>
2023-06-14 12:52:07 -0400

    Add new defines to the mman.h file.

--- libc/inc/sys/mman.h
+++ libc/inc/sys/mman.h
@@ -8,6 +8,13 @@
 
 #include "stddef.h"
 
+#define PROT_READ  0x1 ///< Page can be read.
+#define PROT_WRITE 0x2 ///< Page can be written.
+#define PROT_EXEC  0x4 ///< Page can be executed.
+
+#define MAP_SHARED  0x01 ///< The memory is shared.
+#define MAP_PRIVATE 0x02 ///< The memory is private.
+
 #ifndef __KERNEL__
 
 void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
