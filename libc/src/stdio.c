55b5eb8cee198dabe842fa7d12525f033efbc07a
Enrico Fraccaroli (Galfurian) <enry.frak@gmail.com>
2023-08-11 13:04:34 -0400

    Fix format, and dela with type casting.

--- libc/src/stdio.c
+++ libc/src/stdio.c
@@ -62,7 +62,7 @@ char *gets(char *str)
             }
         } else {
             // The character is stored at address, and the pointer is incremented.
-            *cptr++ = ch;
+            *cptr++ = (char)ch;
         }
     }
     // Add the null-terminating character.
@@ -94,49 +94,46 @@ long strtol(const char *str, char **endptr, int base)
     long acc, cutoff;
     int c;
     int neg, any, cutlim;
-    /*
-     * Skip white space and pick up leading +/- sign if any.
-     * If base is 0, allow 0x for hex and 0 for octal, else
-     * assume decimal; if base is already 16, allow 0x.
-     */
+    // Skip white space and pick up leading +/- sign if any.
+    // If base is 0, allow 0x for hex and 0 for octal, else
+    // assume decimal; if base is already 16, allow 0x.
     s = str;
     do {
         c = (unsigned char)*s++;
     } while (isspace(c));
     if (c == '-') {
         neg = 1;
-        c   = *s++;
+        c   = (int)*s++;
     } else {
         neg = 0;
-        if (c == '+')
-            c = *s++;
+        if (c == '+') {
+            c = (int)*s++;
+        }
     }
     if ((base == 0 || base == 16) &&
         c == '0' && (*s == 'x' || *s == 'X')) {
-        c = s[1];
+        c = (int)s[1];
         s += 2;
         base = 16;
     }
     if (base == 0) {
         base = c == '0' ? 8 : 10;
     }
-    /*
-     * Compute the cutoff value between legal numbers and illegal
-     * numbers.  That is the largest legal value, divided by the
-     * base.  An input number that is greater than this value, if
-     * followed by a legal input character, is too big.  One that
-     * is equal to this value may be valid or not; the limit
-     * between valid and invalid numbers is then based on the last
-     * digit.  For instance, if the range for longs is
-     * [-2147483648..2147483647] and the input base is 10,
-     * cutoff will be set to 214748364 and cutlim to either
-     * 7 (neg==0) or 8 (neg==1), meaning that if we have accumulated
-     * a value > 214748364, or equal but the next digit is > 7 (or 8),
-     * the number is too big, and we will return a range error.
-     *
-     * Set any if any `digits' consumed; make it negative to indicate
-     * overflow.
-     */
+    // Compute the cutoff value between legal numbers and illegal
+    // numbers.  That is the largest legal value, divided by the
+    // base.  An input number that is greater than this value, if
+    // followed by a legal input character, is too big.  One that
+    // is equal to this value may be valid or not; the limit
+    // between valid and invalid numbers is then based on the last
+    // digit.  For instance, if the range for longs is
+    // [-2147483648..2147483647] and the input base is 10,
+    // cutoff will be set to 214748364 and cutlim to either
+    // 7 (neg==0) or 8 (neg==1), meaning that if we have accumulated
+    // a value > 214748364, or equal but the next digit is > 7 (or 8),
+    // the number is too big, and we will return a range error.
+    //
+    // Set any if any `digits' consumed; make it negative to indicate
+    // overflow.
     cutoff = neg ? LONG_MIN : LONG_MAX;
     cutlim = cutoff % base;
     cutoff /= base;
@@ -210,7 +207,7 @@ char *fgets(char *buf, int n, int fd)
         if (c == EOF) {
             break;
         }
-        *p++ = c;
+        *p++ = (char)c;
         if (c == '\n') {
             break;
         }
