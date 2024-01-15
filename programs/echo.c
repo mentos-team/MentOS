6a9451fc0c371312d323849e7e6c27049c699498
Florian Fischer <florian.fischer@muhq.space>
2024-01-15 14:10:04 +0100

    echo: add options -n and -e
    Support suppressing the final newline with the '-n' option.
    Expand backslash escapes when '-e' is specified.
    
    Both options must occur before any other word.
    
    Currently only \n is expanded.

--- programs/echo.c
+++ programs/echo.c
@@ -9,15 +9,71 @@
 
 int main(int argc, char **argv)
 {
+    int newline = 1;
+    int eflag = 0;
+    char buffer[BUFSIZ];
+    char *buf = buffer;
     char *arg;
 
-    // Iterate all words.
+    // Iterate all the words.
     while ((arg = *++argv) != NULL) {
-        puts(arg);
-        if (*(argv+1) != NULL) {
-            putchar(' ');
+        if (arg[0] != '-') {
+            break;
+        }
+
+        // parse the options string
+        while (*arg++) {
+            if (*arg == 'n') {
+                newline = 0;
+            } else if (*arg == 'e') {
+                eflag = '\\';
+            } else {
+                break;
+            }
         }
     }
-    printf("\n");
+
+    // Iterate all remaining words.
+    while ((arg = *argv) != NULL) {
+        // We do not expand escape codes.
+        if (!eflag) {
+            puts(arg);
+            goto next_word;
+        }
+
+        // Expand escape codes
+        int c;
+        while ((c = *arg++) != 0) {
+            if (c != eflag) {
+                *buf++ = c;
+            } else {
+                switch (*arg) {
+                case 'n':
+                    *buf++ = '\n';
+                    break;
+                default:
+                    *buf++ =  *(arg-1);
+                    *buf++ = *arg;
+                }
+                arg++;
+            }
+        }
+        // null-terminate the current word
+        *buf = 0;
+        puts(buffer);
+
+next_word:
+        // Add space if there are more words and the last word did not end with a new line
+        if ((*(argv+1) != NULL) && *(buf-1) != '\n') {
+            putchar(' ');
+        }
+        // reset buf pointer
+        buf = buffer;
+        argv++;
+    }
+
+    if (newline) {
+        printf("\n");
+    }
     return 0;
 }
