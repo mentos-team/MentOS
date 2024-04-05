mentos-team/MentOS

commit 0c398e396e35fd7494b39203297943d260186e30

do not open files during path resolution
Opening the current path component was done because the opened
file could be used for a call to vfs_stat as well as vfs_readlink.

However, a path may contain traversable directories which can not
be opened with vfs_open because they are neither readable nor writeable.

The following path /traversable/foo can not be resolved by uid 1000, if
the traversable directory has the following permissions:

	d--x--x--x 1000 1000 traversable

This is fixed by using vfs_fstat and the current absolute path to
retrieve the attributes of the current path component.

mentos/src/fs/namei.c
@@ -159,28 +159,19 @@ resolve_abspath:
                     *sep_after_cur = 0;
                 }
 
-                vfs_file_t *file;
-                // No permissions are required for path resolution.
-                // Correct permissions of all path components must be
-                // checked by the filesystem implementation.
-                file = vfs_open_abspath(abspath, 0, 0);
-                if (!file) {
-                    // This is the last path component and we want to create
-                    // it anyway.
-                    if (!sep_after_cur && (flags & CREAT_LAST_COMPONENT)) {
+                stat_t statbuf;
+                int ret = vfs_stat(abspath, &statbuf);
+                if (ret < 0) {
+                    // This is the last path component and we want to create it anyway.
+                    if (ret == -ENOENT && !sep_after_cur && (flags & CREAT_LAST_COMPONENT)) {
                         // Just copy the component into the buffer
                         goto copy_path_component;
                     }
-                    return -errno;
+                    return ret;
                 }
-
-                stat_t statbuf;
-                int ret = vfs_fstat(file, &statbuf);
-                if (ret < 0) { vfs_close(file); return ret; }
 
                 // The path component is no symbolic link
                 if (!S_ISLNK(statbuf.st_mode)) {
-                    vfs_close(file);
                     // Restore replaced path separator
                     *sep_after_cur = '/';
                     // Just copy the component into the buffer
@@ -191,8 +182,7 @@ resolve_abspath:
                 if (symlinks > SYMLOOP_MAX) { return -ELOOP; }
 
                 char link[PATH_MAX];
-                ssize_t nbytes = vfs_readlink(file, link, sizeof(link));
-                vfs_close(file);
+                ssize_t nbytes = sys_readlink(abspath, link, sizeof(link));
                 if (nbytes == -1) { return -errno; }
 
                 // Null-terminate link

commit 2a566df1277e9b30cc0c44c9a48502af3c45ce17

namei: do not use vfs_open recursively in resolve_path
In 0c398e396e35fd7494b39203297943d260186e30 I changed the path
resolution to not open all encountered path components, because
directories do not need to be readable by the process to traverse them.

Out of simplicity I erroneously changed the reading of the links from
using the already opened path component to sys_readlink, which internally
calls vfs_open causing an infinite loop.

This is fixed by opening the link to read using vfs_open_abspath
before calling vfs_readlink instead of sys_readlink to replace the
current path component.

Fixes: 0c398e396e35fd7494b39203297943d260186e30

mentos/src/fs/namei.c
@@ -182,7 +182,10 @@ resolve_abspath:
                 if (symlinks > SYMLOOP_MAX) { return -ELOOP; }
 
                 char link[PATH_MAX];
-                ssize_t nbytes = sys_readlink(abspath, link, sizeof(link));
+                vfs_file_t* link_file = vfs_open_abspath(abspath, O_RDONLY, 0);
+                if (link_file == NULL) { return -errno; }
+                ssize_t nbytes = vfs_readlink(link_file, link, sizeof(link));
+                vfs_close(link_file);
                 if (nbytes == -1) { return -errno; }
 
                 // Null-terminate link