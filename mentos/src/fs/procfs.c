bdf95a693484bdf0d505c2437a2b58bab61a9607
Enrico Fraccaroli (Galfurian) <enry.frak@gmail.com>
2024-08-26 11:51:29 -0400

    In procfs fix call to unregister filesystem.

--- mentos/src/fs/procfs.c
+++ mentos/src/fs/procfs.c
@@ -879,7 +879,7 @@ int procfs_cleanup_module(void)
     // Destroy the cache.
     kmem_cache_destroy(fs.procfs_file_cache);
     // Unregister the filesystem.
-    vfs_register_filesystem(&procfs_file_system_type);
+    vfs_unregister_filesystem(&procfs_file_system_type);
     return 0;
 }
 
