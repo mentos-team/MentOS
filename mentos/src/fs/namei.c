mentos-team/MentOS

commit f384cb91f74200ad496e150c60d501a45329d1dd

- Fix how file modes are read;  - In , first check if a path is a link before trying to read the link;

mentos/src/fs/namei.c
@@ -102,17 +102,41 @@ char *realpath(const char *path, char *buffer, size_t buflen)
     return buffer;
 }
 
-static inline int __get_link_content(const char *path, char *link, size_t length)
+/// @brief Determines if the path points to a link.
+/// @param path the path to the file.
+/// @return 1 if it is a link, 0 otherwise.
+static inline int __is_a_link(const char *path)
 {
-    ssize_t link_length = vfs_readlink(path, link, length);
+    stat_t statbuf;
+    if (vfs_stat(path, &statbuf) > 0) {
+        return S_ISLNK(statbuf.st_mode);
+    }
+    return 0;
+}
+
+/// @brief Returns the content of the link.
+/// @param path the path to the file.
+/// @param buffer the buffer where we store the link content.
+/// @param buflen length of the buffer.
+/// @return the length of the link, or a negative value on error.
+static inline int __get_link_content(const char *path, char *buffer, size_t buflen)
+{
+    ssize_t link_length = vfs_readlink(path, buffer, buflen);
     if (link_length < 0) {
         return link_length;
     }
     // Null-terminate link.
-    link[link_length] = 0;
+    buffer[link_length] = 0;
     return link_length;
 }
 
+/// @brief Resolve the path by following all symbolic links.
+/// @param path the path to resolve.
+/// @param buffer the buffer where the resolved path is stored.
+/// @param buflen the size of the provided resolved_path buffer.
+/// @param flags the flags controlling how the path is resolved.
+/// @param link_depth the current link depth.
+/// @return -errno on fail, 1 on success.
 int __resolve_path(const char *path, char *abspath, size_t buflen, int flags, int link_depth)
 {
     char token[NAME_MAX]    = { 0 };
@@ -160,7 +184,7 @@ int __resolve_path(const char *path, char *abspath, size_t buflen, int flags, in
                 pr_err("Buffer overflow while resolving path.\n");
                 return -ENAMETOOLONG;
             }
-            if (flags & FOLLOW_LINKS) {
+            if ((flags & FOLLOW_LINKS) && __is_a_link(buffer)) {
                 ssize_t link_length = __get_link_content(buffer, linkpath, PATH_MAX);
                 if (link_length > 0) {
                     if (link_depth >= SYMLOOP_MAX) {

libc/inc/sys/stat.h
@@ -26,13 +26,14 @@
 /// @defgroup FileTypeTest File Type Test Macros
 /// @brief These macros allows to easily identify file types.
 /// @{
-#define S_ISSOCK(m) (((m) & S_IFMT) == S_IFSOCK) ///< Check if the input values identifies a socket.
-#define S_ISLNK(m)  (((m) & S_IFMT) == S_IFLNK)  ///< Check if the input values identifies a symbolic link.
-#define S_ISREG(m)  (((m) & S_IFMT) == S_IFREG)  ///< Check if the input values identifies a regular file.
-#define S_ISBLK(m)  (((m) & S_IFMT) == S_IFBLK)  ///< Check if the input values identifies a block special.
-#define S_ISDIR(m)  (((m) & S_IFMT) == S_IFDIR)  ///< Check if the input values identifies a directory.
-#define S_ISCHR(m)  (((m) & S_IFMT) == S_IFCHR)  ///< Check if the input values identifies a char special.
-#define S_ISFIFO(m) (((m) & S_IFMT) == S_IFIFO)  ///< Check if the input values identifies a fifo.
+#define S_ISTYPE(mode, mask) (((mode) & S_IFMT) == (mask))
+#define S_ISSOCK(mode)       (S_ISTYPE(mode, S_IFSOCK)) ///< Check if a socket.
+#define S_ISLNK(mode)        (S_ISTYPE(mode, S_IFLNK))  ///< Check if a symbolic link.
+#define S_ISREG(mode)        (S_ISTYPE(mode, S_IFREG))  ///< Check if a regular file.
+#define S_ISBLK(mode)        (S_ISTYPE(mode, S_IFBLK))  ///< Check if a block special.
+#define S_ISDIR(mode)        (S_ISTYPE(mode, S_IFDIR))  ///< Check if a directory.
+#define S_ISCHR(mode)        (S_ISTYPE(mode, S_IFCHR))  ///< Check if a char special.
+#define S_ISFIFO(mode)       (S_ISTYPE(mode, S_IFIFO))  ///< Check if a fifo.
 /// @}
 
 /// @defgroup ModeBitsAccessPermission Mode Bits for Access Permission

mentos/src/fs/ext2.c
@@ -3097,11 +3097,11 @@ static int ext2_fstat(vfs_file_t *file, stat_t *stat)
 /// @return 0 if success.
 static int ext2_stat(const char *path, stat_t *stat)
 {
-    pr_debug("ext2_fstat(path: %s)\n", path);
+    pr_debug("ext2_stat(path: %s)\n", path);
     // Get the EXT2 filesystem.
     ext2_filesystem_t *fs = get_ext2_filesystem(path);
     if (fs == NULL) {
-        pr_err("stat(%s): Failed to get the EXT2 filesystem.\n", path);
+        pr_err("ext2_stat(path: %s): Failed to get the EXT2 filesystem.\n", path);
         return -ENOENT;
     }
     // Prepare the structure for the search.
@@ -3109,13 +3109,13 @@ static int ext2_stat(const char *path, stat_t *stat)
     memset(&search, 0, sizeof(ext2_direntry_search_t));
     // Resolve the path.
     if (ext2_resolve_path(fs->root, path, &search)) {
-        pr_err("stat(%s): Failed to resolve path.\n", path);
+        pr_err("ext2_stat(path: %s): Failed to resolve path.\n", path);
         return -ENOENT;
     }
     // Get the inode associated with the directory entry.
     ext2_inode_t inode;
     if (ext2_read_inode(fs, &inode, search.direntry.inode) == -1) {
-        pr_err("stat(%s): Failed to read the inode of `%s`.\n", path, search.direntry.name);
+        pr_err("ext2_stat(path: %s): Failed to read the inode of `%s`.\n", path, search.direntry.name);
         return -ENOENT;
     }
     /// ID of device containing file.
@@ -3224,7 +3224,7 @@ static ssize_t ext2_readlink(const char *path, char *buffer, size_t bufsize)
         return -ENOENT;
     }
     ssize_t ret = -ENOENT;
-    if ((inode.mode & S_IFLNK) == S_IFLNK) {
+    if (S_ISLNK(inode.mode)) {
         // Get the length of the symlink.
         ret = min(strlen(inode.data.symlink), bufsize);
         // Copy the symlink information.

mentos/src/fs/vfs.c
@@ -394,7 +394,15 @@ vfs_file_t *vfs_creat(const char *path, mode_t mode)
 ssize_t vfs_readlink(const char *path, char *buffer, size_t bufsize)
 {
     pr_debug("vfs_readlink(%s, %s, %d)\n", path, buffer, bufsize);
-    super_block_t *sb = vfs_get_superblock(path);
+    // Allocate a variable for the path.
+    char absolute_path[PATH_MAX] = { 0 };
+    // If the first character is not the '/' then get the absolute path.
+    int ret = resolve_path(path, absolute_path, PATH_MAX, REMOVE_TRAILING_SLASH);
+    if (ret < 0) {
+        pr_err("vfs_readlink(%s, %s, %d): Cannot get the absolute path.", path, buffer, bufsize);
+        return ret;
+    }
+    super_block_t *sb = vfs_get_superblock(absolute_path);
     if (sb == NULL) {
         pr_err("vfs_readlink(%s, %s, %d): Cannot find the superblock!.\n", path, buffer, bufsize);
         return -ENOENT;
@@ -407,7 +415,7 @@ ssize_t vfs_readlink(const char *path, char *buffer, size_t bufsize)
         return -ENOENT;
     }
     // Perform the read.
-    return sb->root->fs_operations->readlink_f(path, buffer, bufsize);
+    return sb->root->fs_operations->readlink_f(absolute_path, buffer, bufsize);
 }
 
 int vfs_symlink(const char *linkname, const char *path)
@@ -444,10 +452,9 @@ int vfs_stat(const char *path, stat_t *buf)
 {
     pr_debug("vfs_stat(path: %s, buf: %p)\n", path, buf);
     // Allocate a variable for the path.
-    char absolute_path[PATH_MAX];
+    char absolute_path[PATH_MAX] = { 0 };
     // If the first character is not the '/' then get the absolute path.
-    int resolve_flags = REMOVE_TRAILING_SLASH | FOLLOW_LINKS;
-    int ret           = resolve_path(path, absolute_path, PATH_MAX, resolve_flags);
+    int ret = resolve_path(path, absolute_path, PATH_MAX, REMOVE_TRAILING_SLASH);
     if (ret < 0) {
         pr_err("vfs_stat(%s): Cannot get the absolute path.", path);
         return ret;
@@ -468,15 +475,7 @@ int vfs_stat(const char *path, stat_t *buf)
         return -ENOSYS;
     }
     // Reset the structure.
-    buf->st_dev   = 0;
-    buf->st_ino   = 0;
-    buf->st_mode  = 0;
-    buf->st_uid   = 0;
-    buf->st_gid   = 0;
-    buf->st_size  = 0;
-    buf->st_atime = 0;
-    buf->st_mtime = 0;
-    buf->st_ctime = 0;
+    memset(buf, 0, sizeof(stat_t));
     // Retrieve the file.
     return sb_root->sys_operations->stat_f(absolute_path, buf);
 }

programs/stat.c
@@ -50,7 +50,7 @@ int main(int argc, char **argv)
     if (S_ISLNK(dstat.st_mode)) {
         char link_buffer[PATH_MAX];
         ssize_t len = readlink(argv[1], link_buffer, sizeof(link_buffer));
-        if (len < 0) {
+        if (len > 0) {
             link_buffer[len] = '\0';
             printf(" -> %s", link_buffer);
         }

commit c0212bd5fed617f99bcadd71f488d6ece826d1d0

fix __is_a_link helper
vfs_stat returns 0 on success, but __is_a_link checked for > 0 and
therefore never detected any symbolic links.

Fixes: f384cb91f74200ad496e150c60d501a45329d1dd

mentos/src/fs/namei.c
@@ -108,7 +108,7 @@ char *realpath(const char *path, char *buffer, size_t buflen)
 static inline int __is_a_link(const char *path)
 {
     stat_t statbuf;
-    if (vfs_stat(path, &statbuf) > 0) {
+    if (vfs_stat(path, &statbuf) == 0) {
         return S_ISLNK(statbuf.st_mode);
     }
     return 0;