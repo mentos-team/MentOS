8cbf1d1fef2db0ba2aca2554af17479d748de08d
Enrico Fraccaroli (Galfurian) <enry.frak@gmail.com>
2026-01-28 14:17:30 +0100

    release: bump version to 0.9.4

--- kernel/inc/version.h
+++ kernel/inc/version.h
@@ -21,7 +21,7 @@
 #define OS_MINOR_VERSION 9
 
 /// Micro version of the operating system.
-#define OS_MICRO_VERSION 3
+#define OS_MICRO_VERSION 4
 
 /// Helper to transform the given argument into a string.
 #define OS_STR_HELPER(x) #x
